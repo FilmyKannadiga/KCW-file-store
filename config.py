4890a4df1fa6f159a99df2483a8349c1e7fd6831
FilmyKannadiga <naveengowdathegreat@gmail.com>
2025-03-24 17:32:51 +0530

    Update config.py

--- config.py
+++ config.py
@@ -60,7 +60,7 @@ VERIFY_MODE = bool(environ.get('VERIFY_MODE', False)) # Set True or False
 
 # If Verify Mode Is True Then Fill All Required Variable, If False Then Don't Fill.
 SHORTLINK_URL = environ.get("SHORTLINK_URL", "ModiJiUrl.com") # shortlink domain without https://
-SHORTLINK_API = environ.get("SHORTLINK_API", "876002d771015cced26a5f5c7dfe5b82a15ea7f4") # shortlink api
+SHORTLINK_API = environ.get("SHORTLINK_API", "") # shortlink api
 VERIFY_TUTORIAL = environ.get("VERIFY_TUTORIAL", "") # how to open link 
 
 # Website Info:
